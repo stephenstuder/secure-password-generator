55fb5dfa1ab9b83611f39bbf124af8056e41338e
stephen studer <stephenkstuder@gmail.com>
2020-03-16 15:37:46 -0700

    added dynamic regex testing

--- script.js
+++ script.js
@@ -1,85 +1,90 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
-  //declaring variables as strings, going to use them like array's
+
   const lowercase = "abcdefghijklmnopqrstuvwxyz";
   const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  const numbers = ["123456789"];
-  const specialCharacters = "#@!%&()/";
-  var characterOptions = "";
-  var securePassword = "";
-  var includesLowerCase, includesUpperCase, includesNumbers, includesSpecialCharacters;
+  const numbers = "123456789";
+  const specialCharacters = "?=.*[!@#$%^&*";
+  let regexTestValues= "";
+  const lowercaseTester = "(?=.*[a-z])";
+  const uppercaseTester = "(?=.*[A-Z])";
+  const numberTester = "(?=.*[0-9])";
+  const specialCharacterTester = "(?=.*[!@#\$%\^&\*])(?=.{8,})";
+  let characterOptions = "";
+  let securePassword = "";
+  let includesLowerCase, includesUpperCase, includesNumbers, includesSpecialCharacters;
 
 
   //getting the password length and not allowing incorrect values
   let passwordLength = 0;
   while (passwordLength < 8 || passwordLength > 128) {
     passwordLength = prompt("enter a number between 8 and 128 for password length.");
+    if (passwordLength < 8){
+      alert("You must select a number greater than 8");
+    } else if (passwordLength > 128){
+      alert("You must select a less than 128");
+    } else if (passwordLength > 8 || passwordLength < 128) {
+      break;
+    } else {
+      alert("Your entry does not appear to be valid, please enter a number between 8 and 128")
+    }
   }
 
-  //figure out what characters they want to include
+  //figure out what characters they want to include and simultaneously build a regex test
   includesLowerCase = confirm("Would you like to include lowercase letter values?");
   if (includesLowerCase) {
     characterOptions += lowercase;
-    console.log(characterOptions);
+    regexTestValues+= lowercaseTester;
   }
   includesUpperCase = confirm("Would you like to include uppercase letter values?");
   if (includesUpperCase) {
     characterOptions += uppercase;
-    console.log(characterOptions);
+    regexTestValues+= uppercaseTester;
   }
   includesNumbers = confirm("Would you like to include numbers values?");
   if (includesNumbers) {
     characterOptions += numbers;
-    console.log(characterOptions);
+    regexTestValues+= numberTester;
   }
   includesSpecialCharacters = confirm("Would you like to include special characters?");
   if (includesSpecialCharacters) {
     characterOptions += specialCharacters;
-    console.log(characterOptions);
+    regexTestValues+= specialCharacterTester;
   }
 
-  //Test that all required values were used, if they werent, start over 
-  //Loop through strings returning a random value of each string
+  //Builds the regex test applicable to the situation created by the user
+  let regexTest = new RegExp(regexTestValues);
+
+  //Loop through pool of requested characters returning a random value each iteration
 
   let passesTest = false;
+  let failCount = 0
 
-  // securePassword = "";
-  for (var i = 0; i < passwordLength; i++) {
-    securePassword += characterOptions.charAt(Math.floor(Math.random() * characterOptions.length))
-
-    switch (securePassword != null) {
-       case includesLowerCase:
-       
-         break;
-       case includesUpperCase:
-        
-           break;
-         case includesNumbers:
-        
-         break;
-       case includesSpecialCharacters:
-    
-         break;
-        default:
-         passesTest = true;
-     }
-
+  // Creates password, test if conditions are met before returning the password
+  while (passesTest != true) {
+    securePassword = "";
+    for (let i = 0; i < passwordLength; i++) {
+      securePassword += characterOptions.charAt(Math.floor(Math.random() * characterOptions.length))
+      //tests that the generated password meets the requirements
+      if (regexTest.test(securePassword)) {
+        passesTest = true;
+      } else {
+        passesTest = false;
+        console.log(failCount++);
+      }
+    }
   } return securePassword;
-
-  //Shuffle and return the result
-
+  
 } 
